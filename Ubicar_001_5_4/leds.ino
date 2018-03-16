7d0a7ffa4bf0594be39d2f6d6699d0ff43d8f42e
RIRI <ririfonfon10@gmail.com>
2018-03-16 12:23:47 +0100

    debug ledblack

--- Ubicar_001_5_4/leds.ino
+++ Ubicar_001_5_4/leds.ino
@@ -12,6 +12,9 @@ void leds_show() {
 }//leds_show
 
 void ledBlack() {
+#ifdef DEBUG
+  Serial.println("ledBlack()");
+#endif
   for (int i = 0 ; i < NUM_LEDS_PER_STRIP ; i++) {
     strands[0]->pixels[i] = pixelFromRGB(0, 0, 0);
     strands[1]->pixels[i] = pixelFromRGB(0, 0, 0);
