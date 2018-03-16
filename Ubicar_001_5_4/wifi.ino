d8d82544a6c0a0ee8de1a95396ca91a61fe07ebc
RIRI <ririfonfon10@gmail.com>
2018-03-16 12:24:42 +0100

    loose wifi all fonction to zero

--- Ubicar_001_5_4/wifi.ino
+++ Ubicar_001_5_4/wifi.ino
@@ -24,6 +24,28 @@ void wifi_event(WiFiEvent_t event) {
   static byte retry = 0;
   static byte maxRetry = 15;
 
+  switch (event) {
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+  master = 0;
+  rr = 0;
+  gg = 0;
+  bb = 0;
+  mod = 0;
+  pix_mod = 0;
+  pix_start = 0;
+  modulo = 0;
+  str_ws = 0;
+  pix_center = 0;
+  led_master = 0;
+  led_mode = 0;
+  led_str = 0;
+  smaster = 0;
+  srr = 0;
+  sgg = 0;
+  sbb = 0;
+      break;
+  }
+
 #ifdef DEBUG
   switch (event) {
     case SYSTEM_EVENT_STA_CONNECTED:
